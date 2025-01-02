fff52bc5b002106ddf7dafcb24eedeb78c1f711c
9381741733 <jyothsnabhukya022@gmail.com>
2025-01-02 19:57:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function stringChop(str, size) {
   // your code here
+	
+  const result = [];
+  
+  for (let i = 0; i < str.length; i += size) {
+    result.push(str.slice(i, i + size));
+  }
+  
+  return result;
+}
+
+
+console.log(chunkString("Hello, world!", 5)); 
+console.log(chunkString("12345", 2));       
+console.log(chunkString("abc", 5));  
 }
 
 // Do not change the code below
