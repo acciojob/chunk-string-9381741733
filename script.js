d8d563496a673dd18d2a33c02c53248a57fa1fa1
9381741733 <jyothsnabhukya022@gmail.com>
2025-01-05 10:25:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 function chunkString(str, chunkLength) {
-  const result = [];
-  
-  for (let i = 0; i < str.length; i += chunkLength) {
-    result.push(str.slice(i, i + chunkLength));
-  }
-  
-  return result;
+    if (chunkLength <= 0) {
+        throw new Error("Chunk length must be greater than 0");
+    }
+
+    const result = [];
+    for (let i = 0; i < str.length; i += chunkLength) {
+        result.push(str.substring(i, i + chunkLength));
+    }
+    return result;
 }
-
-
-
-  
-
-// Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
